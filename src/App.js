59dc3400d57cbaa993dec896dc06965fb259c6c1
Seojune Lee <leeseojune89@gmail.com>
2024-10-11 14:27:48 +0900

    Add Openlayer Map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import "ol";
+import { Map as OlMap, View } from 'ol';
+import { fromLonLat } from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { defaults } from 'ol/control/defaults';
+
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const mapRef = useRef(null);
+  useEffect(() => {
+    const tilelayer = new TileLayer({
+      source: new OSM({ attributions: '' })
+    })
+    const view = new View({
+      center: fromLonLat([129.059556, 35.158282]), // 서면역
+      zoom: 12
+    });
+
+    const map = new OlMap({
+      controls: defaults({ zoom: false, rotate: false, attribution: false }),
+      layers: [
+          tilelayer
+      ],
+      view: view,
+    });
+    map.setTarget(mapRef.current || '');
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapRef} />
     </div>
   );
 }
