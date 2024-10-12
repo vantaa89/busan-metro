70e000513bbb5c0c9601a77b6169b9d7acabb6c1
Seojune Lee <leeseojune89@gmail.com>
2024-10-12 17:59:22 +0900

    Draw line between the stations

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import { OSM } from 'ol/source';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { defaults } from 'ol/control/defaults';
-import { Point } from 'ol/geom';
+import { LineString, Point } from 'ol/geom';
 import { Style, Circle as CircleStyle, Fill, Stroke} from 'ol/style';
 import { Feature } from 'ol';
 import { useState, useEffect, useRef } from 'react';
+import { parse } from 'ol/expr/expression';
 
 function App() {
   const mapRef = useRef(null);
@@ -19,8 +20,15 @@ function App() {
   const [stations, setStations] = useState([])
   const [combinedSource, setCombinedSource] = useState(new VectorSource());
   const [answer, setAnswer] = useState("");
-
-  const lineColor = {"1호선": [240, 106, 0], "2호선": [34, 139, 34], "3호선": [184, 134, 11], "4호선": [30, 144, 255], "동해": [0, 84, 166], "부산김해경전철": [153, 50, 204]};
+  
+  const lineInfo = [
+    {name: "1호선", code: "line1", color: [240, 106, 0]},
+    {name: "2호선", code: "line2", color: [34, 139, 34]},
+    {name: "3호선", code: "line3", color: [184, 134, 11]},
+    {name: "4호선", code: "line4", color: [30, 144, 255]},
+    {name: "동해", code: "donghae", color: [0, 84, 166]},
+    {name: "부산김해경전철", code: "bgl", color: [153, 50, 204]},
+  ];
 
   const buttonPushed = () => {
     console.log(answer);
@@ -34,42 +42,69 @@ function App() {
   }
 
   const loadData = function(){
-    fetch( './data/stations.csv' )
+    let newStations = [];
+    for(let i = 0; i < lineInfo.length; i++){
+      fetch(`./data/${lineInfo[i].code}.csv`)
         .then( response => response.text() )
         .then( responseText => {
           let parsedText = responseText.split("\n").map(e => e.split(","));
-          setStations(stations.concat(parsedText.map(e => ({ name: e[2], lon: e[3], lat: e[4], line: e[1] }))));
-        })
+          newStations = newStations.concat(parsedText.map(e => ({ name: e[2], lon: parseFloat(e[3]), lat: parseFloat(e[4]), line: e[1] })));
+          setStations(newStations);
+        });
+    }          
   };
 
-  useEffect(() => {
+  const drawStations = () => {
     stations.map(station => {
       const pointFeature = new Feature({
         geometry: new Point(fromLonLat([station.lon, station.lat])), 
       });
+      let color;
+      try{
+        color = lineInfo.filter(line => (line.name==station.line))[0].color;
+      } catch {
+        color = 'black';
+      }
       pointFeature.setStyle(
         new Style({
           image: new CircleStyle({
             radius: 3,
             fill: new Fill({ color: 'white' }),
             stroke: new Stroke({
-              color: lineColor[station.line], 
+              color: color,
               width: 2,
             }),
           }),
         })
       );
-
       combinedSource.addFeature(pointFeature);
-
     });
-    console.log(combinedSource);
     setCombinedSource(combinedSource);
-  }, [stations]);
+  }
+
+  const drawLines = () => {
+    for(let i = 0; i < lineInfo.length; i++){
+      const stationsInLine = stations.filter(station => (station.line === lineInfo[i].name));
+      for(let j = 0; j < stationsInLine.length-1; j++){
+        const point1 = fromLonLat([stationsInLine[j].lon, stationsInLine[j].lat]);
+        const point2 = fromLonLat([stationsInLine[j+1].lon, stationsInLine[j+1].lat]);
+        const lineString = new LineString([point1, point2]);
+        const lineFeature = new Feature({
+          geometry: lineString, 
+        });
+        lineFeature.setStyle(new Style({
+          stroke: new Stroke({
+            color: lineInfo[i].color,
+            width: 1.5
+          })
+        }));
+        combinedSource.addFeature(lineFeature);
+        setCombinedSource(combinedSource);
+      }
+    }  
+  }
 
   useEffect(() => {
-
-    loadData();
 
     const tilelayer = new TileLayer({
       source: new OSM({ attributions: '' })
@@ -90,13 +125,16 @@ function App() {
       }),
     });
     map.setTarget(mapRef.current || '');
-  }, [])
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    drawStations();
+    drawLines();
+  }, [stations]);
 
   return (
     <><div className="App">
-      {/* <div>{stations.map(station => {
-      return <p>{station.name}, {station.lat}, {station.lon}</p>
-    })}</div> */}
       <div className="map" ref={mapRef}>
         <div className = "inputbox">
           <input onChange={(e) => setAnswer(e.target.value)} onKeyDown={(e) => keyboardEnter(e)} ref={inputRef}/>
