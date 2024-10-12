aed0919bc73bc31199b7eaf72e3daabf7f61586b
1chan0615 <shin5475612@gmail.com>
2024-10-12 16:57:20 +0900

    Simplify code

--- src/App.js
+++ src/App.js
@@ -9,22 +9,18 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { defaults } from 'ol/control/defaults';
 import { Point } from 'ol/geom';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
-import Feature from 'ol/Feature';
-
+import { Style, Circle as CircleStyle, Fill, Stroke} from 'ol/style';
+import { Feature } from 'ol';
 import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const mapRef = useRef(null);
   const inputRef = useRef(null) 
   const [stations, setStations] = useState([])
-  const [vectorSource1, setVectorSource1] = useState(new VectorSource());
-  const [vectorSource2, setVectorSource2] = useState(new VectorSource());
-  const [vectorSource3, setVectorSource3] = useState(new VectorSource());
-  const [vectorSource4, setVectorSource4] = useState(new VectorSource());
-  const [vectorSourceD, setVectorSourceD] = useState(new VectorSource());
-  const [vectorSourceG, setVectorSourceG] = useState(new VectorSource());
+  const [combinedSource, setCombinedSource] = useState(new VectorSource());
   const [answer, setAnswer] = useState("");
+
+  const lineColor = {"1호선": [240, 106, 0], "2호선": [34, 139, 34], "3호선": [184, 134, 11], "4호선": [30, 144, 255], "동해": [153, 50, 204], "부산김해경전철": [153, 50, 204]};
 
   const buttonPushed = () => {
     console.log(answer);
@@ -47,156 +43,55 @@ function App() {
   };
 
   useEffect(() => {
-    loadData();
-    const tilelayer = new TileLayer({
-      source: new OSM({ attributions: '' })
-    });
-
-
-    const vectorlayer1 = new VectorLayer({
-      source: vectorSource1,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [240, 106, 0], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const vectorlayer2 = new VectorLayer({
-      source: vectorSource2,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [34, 139, 34], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const vectorlayer3 = new VectorLayer({
-      source: vectorSource3,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [184, 134, 11], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const vectorlayer4 = new VectorLayer({
-      source: vectorSource4,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [30, 144, 255], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const vectorlayerD = new VectorLayer({
-      source: vectorSourceD,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [0, 0, 139], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const vectorlayerG = new VectorLayer({
-      source: vectorSourceG,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 3,
-          fill: new Fill({ color: 'white' }),
-          stroke: new Stroke({
-            color: [153, 50, 204], 
-            width: 2,
-          }),
-        }),
-      }),
-    });
-
-    const view = new View({
-      center: fromLonLat([129.059556, 35.158282]), // 서면역
-      zoom: 12
-    });
-
-    const map = new OlMap({
-      controls: defaults({ zoom: false, rotate: false, attribution: false }),
-      layers: [
-          tilelayer,
-          vectorlayer1,
-          vectorlayer2,
-          vectorlayer3,
-          vectorlayer4,
-          vectorlayerD,
-          vectorlayerG
-      ],
-      view: view,
-    });
-    map.setTarget(mapRef.current || '');
-  }, []);
-
-  useEffect(() => {
     stations.map(station => {
       const pointFeature = new Feature({
         geometry: new Point(fromLonLat([station.lon, station.lat])), 
       });
-      /*console.log(`Added ${station.name}, ${station.lon}, ${station.lat}`);*/
-      
-      if(station.line == "1호선"){
-        let newVectorSource = vectorSource1;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSource1(newVectorSource);
-      }
-      if(station.line == "2호선"){
-        let newVectorSource = vectorSource2;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSource2(newVectorSource);
-      }
-      if(station.line == "3호선"){
-        let newVectorSource = vectorSource3;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSource3(newVectorSource);
-      }
-      if(station.line == "4호선"){
-        let newVectorSource = vectorSource4;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSource4(newVectorSource);
-      }
-      if(station.line == "동해"){
-        let newVectorSource = vectorSourceD;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSourceD(newVectorSource);
-      }
-      if(station.line == "부산김해경전철"){
-        let newVectorSource = vectorSourceG;
-        newVectorSource.addFeature(pointFeature);
-        setVectorSourceG(newVectorSource);
-      }
+      // console.log(`Added ${station.name}, ${station.lon}, ${station.lat}`);
+      pointFeature.setStyle(
+        new Style({
+          image: new CircleStyle({
+            radius: 3,
+            fill: new Fill({ color: 'white' }),
+            stroke: new Stroke({
+              color: lineColor[station.line], 
+              width: 2,
+            }),
+          }),
+        })
+      );
+
+      combinedSource.addFeature(pointFeature);
+
     });
-;  }, [stations])
+    console.log(combinedSource);
+    setCombinedSource(combinedSource);
+  }, [stations]);
+
+  useEffect(() => {
+
+    loadData();
+
+    const tilelayer = new TileLayer({
+      source: new OSM({ attributions: '' })
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: combinedSource,
+    });
+    const map = new OlMap({
+      controls: defaults({ zoom: false, rotate: false, attribution: false }),
+      layers: [
+          tilelayer,
+          vectorLayer,
+      ],
+      view: new View({
+        center: fromLonLat([129.059556, 35.158282]), // 서면역
+        zoom: 12
+      }),
+    });
+    map.setTarget(mapRef.current || '');
+  }, [])
 
   return (
     <><div className="App">
